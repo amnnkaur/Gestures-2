acca812aff1f949b5eafc8c7ed283b688f2be66e
Aman Kaur <amankaur2628@gmail.com>
2020-03-12 21:23:12 -0400

    [Aman] Second commit

--- Grstures/PanViewController.swift
+++ Grstures/PanViewController.swift
@@ -67,24 +67,24 @@ class PanViewController: UIViewController {
     
     
     
-    // B a c k g r o u n d     c o l o r
-    
-        override func viewWillAppear(_ animated: Bool) {
-            setGradientBackground()
-            super.viewWillAppear(animated)
-        }
-        
-        func setGradientBackground() {
-            let colorTop =  UIColor(red: 255.0/255.0, green: 149.0/255.0, blue: 0.0/255.0, alpha: 1.0).cgColor
-            let colorBottom = UIColor(red: 255.0/255.0, green: 94.0/255.0, blue: 58.0/255.0, alpha: 1.0).cgColor
-            
-            let gradientLayer = CAGradientLayer()
-            gradientLayer.colors = [colorTop, colorBottom]
-            gradientLayer.locations = [0.0, 1.0]
-            gradientLayer.frame = self.view.bounds
-            
-            self.view.layer.insertSublayer(gradientLayer, at:0)
-        }
+//    // B a c k g r o u n d     c o l o r
+//    
+//        override func viewWillAppear(_ animated: Bool) {
+//            setGradientBackground()
+//            super.viewWillAppear(animated)
+//        }
+//        
+////        func setGradientBackground() {
+//            let colorTop =  UIColor(red: 255.0/255.0, green: 149.0/255.0, blue: 0.0/255.0, alpha: 1.0).cgColor
+//            let colorBottom = UIColor(red: 255.0/255.0, green: 94.0/255.0, blue: 58.0/255.0, alpha: 1.0).cgColor
+//            
+//            let gradientLayer = CAGradientLayer()
+//            gradientLayer.colors = [colorTop, colorBottom]
+//            gradientLayer.locations = [0.0, 1.0]
+//            gradientLayer.frame = self.view.bounds
+//            
+//            self.view.layer.insertSublayer(gradientLayer, at:0)
+//        }
     }
 
 
--- Grstures/PinchViewController.swift
+++ Grstures/PinchViewController.swift
@@ -69,22 +69,22 @@ class PinchViewController: UIViewController {
     
     
     
-    
-
-override func viewWillAppear(_ animated: Bool) {
-           setGradientBackground()
-           super.viewWillAppear(animated)
-       }
-       
-       func setGradientBackground() {
-           let colorTop =  UIColor(red: 155.0/255.0, green: 149.0/255.0, blue: 25.0/255.0, alpha: 1.0).cgColor
-           let colorBottom = UIColor(red: 155.0/255.0, green: 94.0/255.0, blue: 58.0/255.0, alpha: 1.0).cgColor
-           
-           let gradientLayer = CAGradientLayer()
-           gradientLayer.colors = [colorTop, colorBottom]
-           gradientLayer.locations = [0.0, 1.0]
-           gradientLayer.frame = self.view.bounds
-           
-           self.view.layer.insertSublayer(gradientLayer, at:0)
-       }
+//    
+//
+//override func viewWillAppear(_ animated: Bool) {
+//           setGradientBackground()
+//           super.viewWillAppear(animated)
+//       }
+//       
+////       func setGradientBackground() {
+//           let colorTop =  UIColor(red: 155.0/255.0, green: 149.0/255.0, blue: 25.0/255.0, alpha: 1.0).cgColor
+//           let colorBottom = UIColor(red: 155.0/255.0, green: 94.0/255.0, blue: 58.0/255.0, alpha: 1.0).cgColor
+//           
+//           let gradientLayer = CAGradientLayer()
+//           gradientLayer.colors = [colorTop, colorBottom]
+//           gradientLayer.locations = [0.0, 1.0]
+//           gradientLayer.frame = self.view.bounds
+//           
+//           self.view.layer.insertSublayer(gradientLayer, at:0)
+//       }
    }
